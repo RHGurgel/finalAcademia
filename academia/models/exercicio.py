d5ef31ba4bb9079d6e2b587844401df9e16eaa99
FaelPressao <120026787+FaelPressao@users.noreply.github.com>
2025-07-16 22:21:52 -0300

    feat: nova pagina de treino personalizado

--- academia/models/exercicio.py
+++
@@ -1,7 +0,0 @@
-class exercicio():
-    def __init__ (self, nome, descricao, equipamento, tp_treino, video):
-        self.nome = nome
-        self.descricao = descricao
-        self.equipamento = equipamento
-        self.tp_treino = tp_treino
-        self.video = video
--- academia/models/usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario():
-    def __init__(self, nome, sobrenome, email, senha):
-        self.codigo = 0
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.email = email
-        self.senha = senha
-
